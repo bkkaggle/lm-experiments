fa225d183980a766298706724e1a02fea66cda94
Bilal Khan <bk@tinymanager.com>
2019-10-24 08:59:55 -0500

    Add files

---
+++ dataset.py
@@ -0,0 +1,17 @@
+import pickle
+
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset
+
+
+class TextDataset(Dataset):
+    def __init__(self, preprocessed_path):
+        with open(preprocessed_path, "rb") as handle:
+            self.batches = pickle.load(handle)
+
+    def __len__(self):
+        return len(self.batches)
+
+    def __getitem__(self, index):
+        return torch.tensor(self.batches[index])
--- finetune.py
+++ finetune.py
@@ -1,42 +1,24 @@
 import fire
 import glob
 import pickle
+import mlcrate as mlc
 
 import numpy as np
 from tqdm import tqdm
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
-from torch.utils.data import Dataset
+from torch.utils.tensorboard import SummaryWriter
+
+from transformers import CTRLLMHeadModel, CTRLTokenizer, AdamW, WarmupLinearSchedule
+
+from dataset import TextDataset
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-from transformers import CTRLLMHeadModel, CTRLTokenizer
-
-
-def preprocess(
-    path,
-    control_code,
-    save_path="processed_dataset.pkl",
-    checkpoint="ctrl",
-    seq_len=256,
-    subset=False,
-):
-    tokenizer = CTRLTokenizer.from_pretrained(checkpoint)
-
-    examples = []
-    paths = glob.glob(f"{path}/*.txt")
-
-
-def preprocess(
-    path,
-    control_code,
-    save_file="processed_dataset.pkl",
-    checkpoint="ctrl",
-    seq_len=256,
-    subset=False,
-):
+def preprocess(path, control_code, save_file="processed_dataset.pkl", checkpoint="ctrl", seq_len=256, subset=False):
     tokenizer = CTRLTokenizer.from_pretrained(checkpoint)
 
     batches = []
@@ -54,16 +36,72 @@ def preprocess(
 
         tokenized_text = tokenizer.encode(text)
 
-        for i in tqdm(
-            range(0, len(tokenized_text) - seq_len + 1, seq_len),
-            total=int(len(tokenized_text) / seq_len),
-        ):
-            batches.append(
-                tokenizer.encode(control_code) + tokenized_text[i : i + seq_len]
-            )
+        for i in tqdm(range(0, len(tokenized_text) - seq_len + 1, seq_len), total=int(len(tokenized_text) / seq_len),):
+            batches.append(tokenizer.encode(control_code) + tokenized_text[i: i + seq_len])
 
     with open(save_file, "wb") as handle:
         pickle.dump(batches, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+def finetune(checkpoint='ctrl', train_path='./processed_dataset.pkl', learning_rate=1e-5, batch_size=4, epochs=2, gradient_accumulation_steps=1, histogram_interval=100, fp16=True, subset=False):
+    timer = mlc.time.Timer()
+    writer = SummaryWriter()
+
+    writer.add_text('Checkpoint', checkpoint, 0)
+    writer.add_text('Learning rate', str(learning_rate), 0)
+    writer.add_text('Batch size', str(batch_size), 0)
+    writer.add_text('Epochs', str(epochs), 0)
+    writer.add_text('Gradient accumulation steps', str(gradient_accumulation_steps), 0)
+    writer.add_text('Histogram interval', str(histogram_interval), 0)
+
+    train_dataset = TextDataset(train_path)
+    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
+
+    if subset:
+        class Model(nn.Module):
+            def __init__(self):
+                super(Model, self).__init__()
+            
+                self.linear = nn.Linear(256, 1)
+            def forward(self, x, labels):
+                x = self.linear(x)
+
+                loss = F.cross_entropy(x.view(-1, 256), labels.view(-1, 256))
+
+                return (loss, x)
+
+        model = Model().to(device)
+    else:
+        model = CTRLLMHeadModel.from_pretrained(checkpoint).to(device)
+
+    no_decay = ['bias', 'LayerNorm.weight']
+    optimizer_grouped_parameters = [
+        {'params': [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)], 'weight_decay': 0.0},
+        {'params': [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+    ]
+
+    train_steps = int(len(train_dataloader) / gradient_accumulation_steps * epochs)
+    optimizer = AdamW(optimizer_grouped_parameters, lr=learning_rate, eps=1e-8)
+    scheduler = WarmupLinearSchedule(optimizer, warmup_steps=int(0.1 * train_steps), t_total=train_steps)
+
+    if fp16:
+        from apex import amp
+
+        model, optimizer = amp.initialize(model, optimizer, opt_level='O1')
+
+    for epoch in range(epochs):
+        print(f'Epoch: {epoch}')
+        timer.add(epoch)
+
+        train_loss = 0
+
+        model.train()
+        for i, batch in tqdm(enumerate(train_dataloader), total=(int(len(train_dataset) / batch_size / gradient_accumulation_steps))):
+            inputs, labels = batch.to(device), batch.to(device)
+
+            out = model(inputs, labels=labels)
+
+
+
 
 
 if __name__ == "__main__":
